3e1d66a785251a951830a1f2ee63b690dbd1d748
dkharshiladze <davit.kharshiladze88@gmail.com>
2022-09-08 22:01:24 +0400

    new change

--- src/test/java/DataProviderTest.java
+++ src/test/java/DataProviderTest.java
@@ -7,7 +7,7 @@ public class DataProviderTest {
 
         @DataProvider
         public Object[][] dpMethod(){
-            return new Object[][] {{2, 3 , 5}, {5, 7, 1}};
+            return new Object[][] {{2, 3 , 5}, {5, 7, 14}};
         }
 
         @Test(dataProvider = "dpMethod")
